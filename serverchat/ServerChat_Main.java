9d90869e17112af6df04d996d14de91c23ccab0a
Angelo Scalabroni <armotek5421@msn.com>
2014-07-06 13:11:07 -0400

    Added Threading, server - client working with run arguments

--- serverchat/ServerChat_Main.java
+++ serverchat/ServerChat_Main.java
@@ -21,28 +21,67 @@ public class ServerChat_Main {
      */
     private String hostname;
     public String username;
-    
-    
+    private final static int DEFAULT_SERVER_PORT = 15750;
+    private final static String SERVER_IP = "192.168.0.9";
+    private ChatClient client = null;
+
     public static void main(String[] args) {
-        ServerChat_Main program = new ServerChat_Main();
+        if (args.length == 2) {
+            if (args[0].equalsIgnoreCase("server") && (ServerChat_Main.isInt(args[1]) && Integer.parseInt(args[1]) > 1023)) {
+                new ChatServer(Integer.parseInt(args[1]));
+            } else {
+                System.out.println("Usage: java ServerChat_Main server port(>1023)");
+                System.out.println("or...java ServerChat_Main without arguments for chat client");
+                System.exit(0);
+            }
+        } else if (args.length == 1) {
+            if (args[0].equalsIgnoreCase("server")) {
+                /* User indicated that this computer
+                 * should be used as a server with
+                 * the default port 15750 used
+                 */
+                new ChatServer(DEFAULT_SERVER_PORT);
+            } else {
+                System.out.println("Usage: java ServerChat_Main server port(>1023)");
+                System.out.println("or...java ServerChat_Main without arguments for chat client");
+                System.exit(0);
+            }
+        } else {
+            // open the client for user
+            new ServerChat_Main();
+        }
+//        program.initServer(15750);
     }
-    
-    
-    public ServerChat_Main(){
+
+    public ServerChat_Main() {
         try {
             this.hostname = InetAddress.getLocalHost().getHostName();
         } catch (UnknownHostException ex) {
             Logger.getLogger(ServerChat_Main.class.getName()).log(Level.SEVERE, null, ex);
         }
-            System.out.println("Welcome to my chat program..");
-            System.out.println("Your hostname is: " + this.hostname);
-            this.setUsername();
+        System.out.println("Welcome to the ServerChat Client");
+        System.out.println("Your hostname is: " + this.hostname);
+        this.setUsername();
+        client = new ChatClient(SERVER_IP, DEFAULT_SERVER_PORT);
     }
-    
-    public void setUsername(){
+
+    public void setUsername() {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter your on-screen username: ");
         this.username = input.nextLine();
         System.out.println("Welcome " + this.username);
+    }
+
+    private void initServer(int port) {
+        new ChatServer(port);
+    }
+
+    public static boolean isInt(String string) {
+        try {
+            Integer.parseInt(string);
+        } catch (NumberFormatException nFE) {
+            return false;
+        }
+        return true;
     }
 }
